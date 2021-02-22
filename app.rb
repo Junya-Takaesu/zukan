51d9cd09418f99c82bd50337834bdc2d91a6c837
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-02-22 23:36:38 +0900

    Hover effects and etc

--- app.rb
+++ app.rb
@@ -3,9 +3,11 @@ require "sinatra/reloader"
 require_relative "database/pokemons"
 require_relative "database/moves"
 
+set :static_cache_control, [:public, :max_age => 31536000]
+
 get "/" do
   pokemons = Pokemons.new
-  @pokemons = pokemons.select.to_a.shuffle
+  @pokemons = pokemons.select.to_a
   erb :index
 end
 
