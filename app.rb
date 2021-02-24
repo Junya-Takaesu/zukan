f1652236632fb746026793fc1ddb439e590627f8
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-02-24 14:13:44 +0900

    Update app because tables are added to the app

--- app.rb
+++ app.rb
@@ -1,16 +1,30 @@
 require "sinatra"
-require "sinatra/reloader"
-require_relative "database/pokemons"
-require_relative "database/moves"
+require_relative "models/pokemon"
+require_relative "models/ability"
+require_relative "models/type"
+require_relative "models/move"
+require_relative "db/database_booter.rb"
+
+if development?
+  require "sinatra/reloader"
+end
+
+unless ENV["PORT"].nil?
+  set :port, ENV["PORT"]
+end
+
+db_booter = DatabaseBooter.new
+if Pokemon.all.count == 0 && Ability.all.count == 0 && Type.all.count == 0 && Move.all.count == 0
+  db_booter.migrate
+end
 
 set :static_cache_control, [:public, :max_age => 31536000]
 
 get "/" do
-  pokemons = Pokemons.new
-  @pokemons = pokemons.select.to_a
+  @pokemons = Pokemon.all
   erb :index
 end
 
-post "/" do
+get "/:pokemon_no" do
   erb :detail
 end
