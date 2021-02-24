d8df14e1a704c893884904a04d141e5462cb6e29
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-02-24 10:23:08 +0900

    Add new tables

--- db/database_booter.rb
+++ db/database_booter.rb
@@ -1,7 +1,9 @@
 require "json"
 require_relative "schema"
-require_relative "../models/pokemon.rb"
-require_relative "../models/move.rb"
+require_relative "../models/pokemon"
+require_relative "../models/ability"
+require_relative "../models/type"
+require_relative "../models/move"
 
 class DatabaseBooter
 
@@ -9,7 +11,8 @@ class DatabaseBooter
   BULK_INSERT_MAX = 100;
 
   def initialize
-    Schema.create_tables
+    schema = Schema.new
+    schema.up
     @json_files = Dir[JSONS_GLOB]
   end
 
@@ -19,40 +22,58 @@ class DatabaseBooter
       parsed_json = JSON.parse json
 
       pokemons = []
+      abilities = []
+      types = []
       moves = []
 
       parsed_json.each do |pokemon|
         pokemons.push({
           pokemon_no: pokemon["no"],
           name: pokemon["name"],
-          stage: pokemon["stage"],
-          abilities: pokemon["abilities"],
-          types: pokemon["types"]
+          stage: pokemon["stage"]
         })
+
+        pokemon["abilities"].each do |ability|
+          abilities.push({
+            pokemon_no: pokemon["no"],
+            ability: ability
+          })
+        end
+
+        pokemon["types"].each do |type|
+          types.push({
+            pokemon_no: pokemon["no"],
+            type: type
+          })
+        end
 
         Move::TYPES.each do |type|
           pokemon[type].each do |move|
             (index, move_name) = move
             moves.push({
-              no: pokemon["no"],
-              move_type: type,
+              pokemon_no: pokemon["no"],
+              type: type,
               name: move_name
             })
           end
         end
 
         if BULK_INSERT_MAX <= pokemons.length
-          pp "[#{__FILE__}] Inserting #{pokemons.length} records from #{json_file}"
-          buffers = {Pokemon: pokemons, Move: moves}
+          announce_bulk_insert("pokemons", pokemons.length, json_file)
+          announce_bulk_insert("abilities", abilities.length, json_file)
+          announce_bulk_insert("types", types.length, json_file)
+          announce_bulk_insert("moves", moves.length, json_file)
+          buffers = {Pokemon: pokemons, Ability: abilities, Type: types,  Move: moves}
           insert_buffers buffers
 
           pokemons = []
+          abilities = []
+          types = []
           moves = []
         end
       end
 
       if 0 < pokemons.length
-        pp "[#{__FILE__}] Inserting #{pokemons.length} records from #{json_file}"
         Pokemon.insert_all(pokemons)
         Move.insert_all(moves)
       end
@@ -64,6 +85,10 @@ class DatabaseBooter
       model = Module.const_get class_name
       model.insert_all(buffer)
     end
+  end
+
+  def announce_bulk_insert(table_name, records_count, file_name)
+    puts "[#{__FILE__}] Inserting into #{table_name}: #{records_count} records from #{file_name}"
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -1,44 +1,95 @@
 require 'active_record'
 require_relative 'database'
 
-class Schema
+class Schema < ActiveRecord::Migration[6.0]
   include Database
-
-  def self.create_tables
-    Database.establish_connection
-
-    ActiveRecord::Schema.define do
-      unless table_exists?(:pokemons)
-        create_table :pokemons, force: :cascade do |t|
-          t.integer :pokemon_no, null: false
-          t.string  :name,       null: false
-          t.integer :stage
-          t.string  :abilities
-          t.string  :types
-        end
-        add_index :pokemons, :pokemon_no, unique: true
+  Database.establish_connection
+  def up
+    unless table_exists?(:pokemons)
+      create_table :pokemons, force: :cascade do |t|
+        t.integer :pokemon_no, null: false
+        t.string  :name,       null: false
+        t.integer :stage
+        t.string  :abilities
+        t.string  :types
       end
+      add_index :pokemons, :pokemon_no, unique: true
+    end
 
-      unless table_exists?(:moves)
-        create_table :moves, force: :cascade do |t|
-          t.integer :no
-          t.string  :move_type
-          t.string  :name
-        end
+    unless table_exists?(:moves)
+      create_table :moves, force: :cascade do |t|
+        t.integer :pokemon_no
+        t.string  :type
+        t.string  :name
       end
     end
 
-    alter_table_add_foreign_key = %(
-      ALTER TABLE moves
-      ADD CONSTRAINT fk_pokemon_no
-      FOREIGN KEY (no)
-      REFERENCES pokemons(pokemon_no)
-      ON DELETE CASCADE;
-    )
-    ActiveRecord::Base.connection.execute(alter_table_add_foreign_key)
+    unless table_exists?(:abilities)
+      create_table :abilities, force: :cascade do |t|
+        t.integer :pokemon_no
+        t.string  :ability
+      end
+    end
+
+    unless table_exists?(:types)
+      create_table :types, force: :cascade do |t|
+        t.integer :pokemon_no
+        t.string  :type
+      end
+    end
+
+    unless foreign_key_exists?(:moves, name: "fk_pokemon_no")
+      alter_table_add_foreign_key = %(
+        ALTER TABLE moves
+        ADD CONSTRAINT fk_pokemon_no
+        FOREIGN KEY (pokemon_no)
+        REFERENCES pokemons(pokemon_no)
+        ON DELETE CASCADE;
+      )
+      ActiveRecord::Base.connection.execute(alter_table_add_foreign_key)
+    end
+    unless foreign_key_exists?(:abilities, name: "fk_pokemon_no")
+      alter_table_add_foreign_key = %(
+        ALTER TABLE abilities
+        ADD CONSTRAINT fk_pokemon_no
+        FOREIGN KEY (pokemon_no)
+        REFERENCES pokemons(pokemon_no)
+        ON DELETE CASCADE;
+      )
+      ActiveRecord::Base.connection.execute(alter_table_add_foreign_key)
+    end
+    unless foreign_key_exists?(:types, name: "fk_pokemon_no")
+      alter_table_add_foreign_key = %(
+        ALTER TABLE types
+        ADD CONSTRAINT fk_pokemon_no
+        FOREIGN KEY (pokemon_no)
+        REFERENCES pokemons(pokemon_no)
+        ON DELETE CASCADE;
+      )
+      ActiveRecord::Base.connection.execute(alter_table_add_foreign_key)
+    end
+  end
+
+  def down
+    ActiveRecord::Base.connection.execute("drop table pokemons cascade")
+    ActiveRecord::Base.connection.execute("drop table moves cascade")
+    ActiveRecord::Base.connection.execute("drop table abilities cascade")
+    ActiveRecord::Base.connection.execute("drop table types cascade")
   end
 end
 
 if $0 == __FILE__
-  Schema.create_tables
+  schema = Schema.new
+  error_message = "up か down を指定してください"
+  pp error_message if ARGV.empty?
+  ARGV.each do |arg|
+    case arg
+    when "up"
+      schema.up
+    when "down"
+      schema.down
+    else
+      pp error_message
+    end
+  end
 end
