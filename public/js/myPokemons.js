2ae9e200d4faf6ad04ebfcb4fd9ff1332e8579a2
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-03-04 21:45:34 +0900

    Display my pokemon counter

--- public/js/myPokemons.js
+++ public/js/myPokemons.js
@@ -2,6 +2,9 @@
 
   const pokemonNos = JSON.parse(localStorage.getItem("myPokemons"));
 
+  const myPokemonCounter = document.querySelector('.my-pokemon-stats p');
+  myPokemonCounter.innerText = `${pokemonNos.length} ${myPokemonCounter.innerText}`;
+
   pokemonNos.forEach(pokemonNo => {
     const id = String(pokemonNo).padStart(3, "0");
     pokemonDom = document.getElementById(id);
