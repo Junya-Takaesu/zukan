532d1efaf6bb0f92125e5c0e15239f6c699a6609
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-03-08 15:45:52 +0900

    Add unit test code for a model

--- models/pokemon.rb
+++ models/pokemon.rb
@@ -5,3 +5,7 @@ class Pokemon < ApplicationRecord
   has_many :abilities, foreign_key: "pokemon_no", primary_key: "pokemon_no"
   has_many :types, foreign_key: "pokemon_no", primary_key: "pokemon_no"
 end
+
+if $0 == __FILE__
+  # Pokemon.joins(:moves).where("pokemons.pokemon_no" => 13).each
+end
