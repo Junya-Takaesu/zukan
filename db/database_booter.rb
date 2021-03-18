3db9128fd08a20b550f0a626dd13c665c17d56cc
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-03-18 20:14:27 +0900

    Import json to tables and join tables

--- db/database_booter.rb
+++ db/database_booter.rb
@@ -1,5 +1,5 @@
 require "json"
-require_relative "../models/application_record.rb"
+require_relative "../models/application_record"
 
 class DatabaseBooter
 
@@ -13,51 +13,105 @@ class DatabaseBooter
   end
 
   def migrate
-    pokemons = []
-    abilities = []
-    types = []
-    moves = []
+    ActiveRecord::Base.transaction do
+      pokemons = []
+      abilities = []
+      types = []
+      moves = []
+
+      @json_files.each do |json_file|
+        json = File.read json_file
+        parsed_json = JSON.parse json
+
+        parsed_json.each do |pokemon|
+          pokemons.push({
+            pokemon_no: pokemon["no"],
+            name: pokemon["name"],
+            stage: pokemon["stage"]
+          })
+
+          pokemon["abilities"].each do |ability|
+            abilities.push({
+              ability_name: ability
+            })
+          end
+
+          pokemon["types"].each do |type|
+            types.push({
+              type_name: type
+            })
+          end
+
+          Move::TYPES.each do |move_type|
+            pokemon[move_type].each do |move|
+              (index, move_name) = move
+              moves.push({
+                move_name: move_name
+              })
+            end
+          end
+        end
+      end
+
+      abilities = abilities.index_by {|row| row[:ability_name]}.values
+      types = types.index_by {|row| row[:type_name]}.values
+      moves = moves.index_by {|row| row[:move_name]}.values
+
+      buffers = {Pokemon: pokemons, Ability: abilities, Type: types,  Move: moves}
+      insert_buffers buffers
+      migrate_join_tables
+    end
+  end
+
+  private
+
+  def migrate_join_tables
+    pokemons_abilities = []
+    pokemons_types = []
+    pokemons_moves = []
+
+    abilities = Ability.all.pluck(:ability_name, :id).to_h
+    types = Type.all.pluck(:type_name, :id).to_h
+    moves = Move.all.pluck(:move_name, :id).to_h
 
     @json_files.each do |json_file|
       json = File.read json_file
       parsed_json = JSON.parse json
 
       parsed_json.each do |pokemon|
-        pokemons.push({
-          pokemon_no: pokemon["no"],
-          name: pokemon["name"],
-          stage: pokemon["stage"]
-        })
+
+        pokemon_no = pokemon["no"]
 
         pokemon["abilities"].each do |ability|
-          abilities.push({
-            ability_name: ability
+          pokemons_abilities.push({
+            pokemon_no: pokemon_no,
+            ability_id: abilities[ability]
           })
         end
 
         pokemon["types"].each do |type|
-          types.push({
-            type_name: type
+          pokemons_types.push({
+            pokemon_no: pokemon_no,
+            type_id: types[type]
           })
         end
 
         Move::TYPES.each do |move_type|
           pokemon[move_type].each do |move|
             (index, move_name) = move
-            moves.push({
-              move_name: move_name
+            pokemons_moves.push({
+              pokemon_no: pokemon_no,
+              move_type: "'#{move_type}'",
+              move_id: moves[move_name]
             })
           end
         end
       end
     end
 
-    abilities = abilities.index_by {|row| row[:ability_name]}.values
-    types = types.index_by {|row| row[:type_name]}.values
-    moves = moves.index_by {|row| row[:move_name]}.values
-
-    buffers = {Pokemon: pokemons, Ability: abilities, Type: types,  Move: moves}
-    insert_buffers buffers
+    insert_raw_sql(table = "pokemons_abilities", pokemons_abilities)
+    insert_raw_sql(table = "pokemons_types", pokemons_types)
+    insert_raw_sql(table = "pokemons_moves", pokemons_moves)
   end
 
   def insert_buffers(buffers = {})
@@ -65,6 +119,22 @@ class DatabaseBooter
       model = Module.const_get class_name
       model.insert_all(buffer)
     end
+  end
+
+  def insert_raw_sql(table, rows = [])
+    columns = get_keys(rows)
+    values = []
+    sql = "INSERT INTO #{table} (#{columns.join(",")}) VALUES "
+
+    rows.each do |row|
+      values.push "(#{row.values.join(",")})"
+    end
+
+    ActiveRecord::Base.connection.execute(sql + values.join(","))
+  end
+
+  def get_keys(rows)
+    (rows.index_by {|r| r.keys}.values)[0].keys
   end
 end
 
