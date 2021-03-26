d2e5f8a019d074b55732b30c6a154c68757042ba
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-03-26 23:16:52 +0900

    Add style to go to top button

--- public/js/common/goToTopButton.js
+++ public/js/common/goToTopButton.js
@@ -1,13 +1,15 @@
 const createGoToTopAnchor = () => {
-
+  const containerDiv = document.createElement("div");
   const goToTopAnchor = document.createElement("a");
-  const buttonGraphic = document.createElement("div");
-  buttonGraphic.classList.add("monster-ball-button");
+  const switchDrawing = document.createElement("div");
+  const buttonLabel = document.createElement("p");
 
-  goToTopAnchor.append(buttonGraphic);
-
+  containerDiv.id = "go-to-top-container"
   goToTopAnchor.id = "go-to-top";
   goToTopAnchor.href = "#";
+  switchDrawing.classList.add("monster-ball-button");
+  buttonLabel.id = "go-to-top-label";
+  buttonLabel.innerHTML = "トップに<br>もどる"
 
   document.addEventListener("scroll", () => {
     if(100 < window.scrollY) {
@@ -15,7 +17,10 @@ const createGoToTopAnchor = () => {
     }
   })
 
-  return goToTopAnchor;
+  goToTopAnchor.append(switchDrawing);
+  containerDiv.append(buttonLabel);
+  containerDiv.append(goToTopAnchor);
+  return containerDiv;
 }
 
 export {createGoToTopAnchor}
