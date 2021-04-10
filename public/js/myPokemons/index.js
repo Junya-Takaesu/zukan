4f176bc9493812908c1ccf4a0f6c4f26c0ddae17
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-04-10 15:58:34 +0900

    Fix bug showing no number in my pokemon

--- public/js/myPokemons/index.js
+++ public/js/myPokemons/index.js
@@ -2,11 +2,12 @@ import {CustomModal} from "./customModal.js";
 import {createGoToTopAnchor} from "../common/goToTopButton.js";
 
 document.addEventListener("DOMContentLoaded", ()=>{
-  const pokemonNos = JSON.parse(localStorage.getItem("myPokemons"));
+  const pokemonNos = JSON.parse(localStorage.getItem("myPokemons")) ?? [];
   const customModal = new CustomModal(pokemonNos);
+  const countMyPokemon = pokemonNos.length;
 
   const myPokemonCounter = document.querySelector('.my-pokemon-stats p');
-  myPokemonCounter.innerText = `${pokemonNos.length} ${myPokemonCounter.innerText}`;
+  myPokemonCounter.innerText = `${countMyPokemon} ${myPokemonCounter.innerText}`;
 
   pokemonNos.forEach(pokemonNo => {
     const id = String(pokemonNo).padStart(3, "0");
@@ -15,6 +16,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const img = pokemonDom.querySelector("img");
     img.src = `images/${id}.png`;
   })
-  customModal.initializeModals();
+  if (0 < countMyPokemon) {
+    customModal.initializeModals();
+  }
   document.querySelector("body").append(createGoToTopAnchor());
 })
